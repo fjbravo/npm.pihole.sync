65153add61afd59f1d52432168b68ee56fe2e693
Felix J Bravo <fjbravo@users.noreply.github.com>
2024-04-03 22:26:39 -0400

    - added the optionto do a dry_run

--- npm.pihole.sync.sh
+++ npm.pihole.sync.sh
@@ -6,18 +6,22 @@ PIHOLE_IP=${PIHOLE_IP}
 NPM_SERVER_IP=${NPM_SERVER_IP}
 PIHOLE_USER=${PIHOLE_USER}
 PIHOLE_PASS=${PIHOLE_PASS}
+DRY_RUN=${DRY_RUN:-0} # Default to 0 (false) if not set
 
 # Function to add DNS record to Pi-hole
 add_dns_record() {
     local server_name=$1
     local ip_address=$2
+    local dry_run=$3
 
     # Check if the record already exists
     if curl -s -L -X GET "http://${PIHOLE_IP}/admin/api_db.php?get=domains" --data "auth=${PIHOLE_PASS}" | jq -e ".data[] | select(.domain == \"${server_name}\")" > /dev/null; then
         echo "Record for ${server_name} already exists in Pi-hole."
     else
         # Add the DNS record
-        curl -s -L -X POST "http://${PIHOLE_IP}/admin/api_db.php?add=domain" --data "domain=${server_name}&ip=${ip_address}&auth=${PIHOLE_PASS}"
+        if [[ "${dry_run}" -eq 0 ]]; then
+            curl -s -L -X POST "http://${PIHOLE_IP}/admin/api_db.php?add=domain" --data "domain=${server_name}&ip=${ip_address}&auth=${PIHOLE_PASS}"
+        fi
         echo "Added record for ${server_name} to Pi-hole."
     fi
 }
